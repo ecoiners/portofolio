48cd612444d513b36114290963a611d4f94b1e96
ecoiners <damuna63@gmail.com>
2025-09-06 06:29:11 +0700

    src/components/Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+import {aurion} from "../assets/aurion.png";
 
 export default function Header() {
 	
@@ -5,7 +6,7 @@ export default function Header() {
 		<div className="fixed top-0 z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
 		  <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
 			  <a className="flex items-center gap-2 w-auto xl:mr-8" href="/">
-				  <img src="../assets/aurion.png" alt="aurion logo" className="h-10 w-10 object-contain bg-transparent"/>
+				  <img src={aurion} alt="aurion logo" className="h-10 w-10 object-contain bg-transparent"/>
 					<span className="text-xl font-bold text-gold-2">
 					  AURION
 					</span>
