c6ef0bf94272aec3c5cb96a99cc8b6eeaec1121b
ecoiners <damuna63@gmail.com>
2025-09-06 07:30:05 +0700

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,9 +5,11 @@ export default function Header() {
 	return (
 		<div className="fixed top-0 z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
 		  <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
-			  <a className="block items-center gap-2 w-[12rem] xl:mr-8" href="/">
-				  <img src={aurion} alt="aurion "/>
-					
+			  <a className="block items-center w-[12rem] xl:mr-8" href="/">
+				  <span className="flex items-center gap-2 text-xl text-gold-2">
+					  <img src={aurion} alt="aurion" className="h-10 w-10 " />
+						AURION
+					</span>
 				</a>
 				
 				<nav>
